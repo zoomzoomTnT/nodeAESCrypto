87dee5a52278c05b4c8928ea00ef240c11b1eaaa
Zoomzoom Zee <z.zcong677@gmail.com>
2018-06-07 17:11:24 -0500

    fix small

--- CINR.js
+++ CINR.js
@@ -2,28 +2,31 @@
 const crypto = require("crypto");
 const eccrypto = require("eccrypto");
 
-let isSigned = false;
 
-const userInfo = function (email) {
+const userInfo = function (email) { //acts like a user account data
+    /*private fields*/
     let name = email.split('@', 1),
         password = "0000",
         privateKey = crypto.randomBytes(32),
         hashEmail = crypto.createHash("sha256").update(email).digest(),
         signature = null,
         inbox = [];
+
+    /*public fields and method*/
     return {
         publicKey: eccrypto.getPublic(privateKey), // Corresponding uncompressed (65-byte) public key.
-        login: function(pw, callback){
+
+        login: function(pw, callback){ //provide credential to access private fields
             if(pw === password) {
-                callback({
+                callback({              //what was private now is visible in callback function
                     name: name,
                     privateKey: privateKey,
                     hashEmail: hashEmail,
                     inbox: inbox,
-                    setSig: function(sig) {
+                    setSig: function(sig) { //enable updating its own signature
                         signature = sig;
                     },
-                    sendEmail: function (to, callback) {
+                    sendEmail: function (to, callback) { //Send a digital signature payload containing few information to someone else
                         callback({
                             content: "email text",
                             hash: hashEmail,
@@ -32,41 +35,41 @@ const userInfo = function (email) {
                     }
                 });
             } else {
-                console.log("log in fail");
+                console.log("log in fail"); //no need for fail call back
             }
         },
 
-        verify: function() {
-
-        },
-
-        sendToInbox: function(emailData) {
+        sendToInbox: function(emailData) {//set function for inbox arr
             inbox[0] = emailData;
         }
-
-
     }
 
 };
 
-function createSig() {
+function createSig() {// signing process for userA
+    /*Log in with password*/
     userA.login("0000", function (data) {
+        /*be able to access all private info and sign with private key*/
         eccrypto.sign(data.privateKey, data.hashEmail)
-            .then(function(sig) {
+            .then (function(sig) {
                 console.log("Signature in DER format:", sig);//Authentication
                 data.setSig(sig);
-            })
-            .then (data.sendEmail(userB, function (sigdata) {
+                /*send email and sig to userB*/
+                data.sendEmail(userB, function (sigdata) {
                     userB.sendToInbox(sigdata);
-                    })
-            );
+                });
+
+            });
+
 
 
     });
 }
 
-function verifySig() {
+function verifySig() {// verifying process for userB
+    /*Log in with password*/
     userB.login("0000", function (data) {
+        /*be able to access all private info and grab inbox emails*/
         eccrypto.verify(userA.publicKey, data.inbox[0].hash, data.inbox[0].sig) //Non- Repudiation
             .then(function() {
                 console.log("Signature is OK");
@@ -77,11 +80,19 @@ function verifySig() {
     })
 }
 
+
+/*testing*/
 let userA = userInfo("z.zcong677@gmail.com");
 let userB = userInfo("sam@gmail.com");
 createSig();
-
 setTimeout(verifySig, 500);
+/*
+"C:\Program Files\JetBrains\WebStorm 2018.1.3\bin\runnerw.exe" "C:\Program Files\nodejs\node.exe" C:\Users\zzcon\WebstormProjects\nodeAES\CINR.js
+Signature in DER format: <Buffer 30 45 02 21 00 f2 6c f3 81 6d 0a 53 40 17 a0 e3 bb d4 ac 5d 14 28 96 16 88 ae c0 10 1e 92 5d d7 a6 f4 bd 6d 06 02 20 63 09 d9 fe 5b 88 2a e4 b4 aa 80 ... >
+Signature is OK
+
+Process finished with exit code 0
+* */
 
 
 
